e8daeb5f0779026ae63683b7b6f975cb3ccb766b
cha7713 <cha7713@gmail.com>
2020-07-18 14:45:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/navigator/SubFragment.java
@@ -0,0 +1,39 @@
+package com.example.navigator;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SubFragment extends Fragment {
+
+    public SubFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sub, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String text = SubFragmentArgs.fromBundle(getArguments()).getVarr(); //rebuild를 한번 해줘야 넘긴값이 잘 생성됨
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText(text);
+    }
+}
